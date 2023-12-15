3bc2417ee7332d376a615575015c340e500ef2c8
stationAstral <cougarjonez@gmail.com>
2023-12-14 23:31:01 -0800

    Through #6

---
+++ Hello Pizza/MenuItemView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuItemView.swift
+//  Hello Pizza
+//
+//  Created by stationAstral on 12/14/23.
+//
+
+import SwiftUI
+
+struct MenuItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuItemView()
+}
