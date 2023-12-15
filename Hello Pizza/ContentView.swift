dc001d60ab7d7375aebcf410052961cfd5f47bbd
stationAstral <cougarjonez@gmail.com>
2023-12-14 20:53:40 -0800

    Initial Commit

---
+++ Hello Pizza/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hello Pizza
+//
+//  Created by stationAstral on 12/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hello Pizza/Hello_PizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_PizzaApp.swift
+//  Hello Pizza
+//
+//  Created by stationAstral on 12/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Hello_PizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
